59d8520793365af361753b61ffc4e2150e0f67f0
Stefan Simon <stefan.j.c.simon@gmail.com>
2024-11-11 20:26:45 +0100

    [+] Json support

--- portal-env/src/main/java/org/example/config/RabbitMqConfig.java
+++ portal-env/src/main/java/org/example/config/RabbitMqConfig.java
@@ -1,6 +1,8 @@
 package org.example.config;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,8 +10,14 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMqConfig {
 
     @Bean
-    public Queue senderOptions() {
-        return new Queue( "analysis.classic", true );
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        var template = new RabbitTemplate( connectionFactory );
+        template.setMessageConverter( new Jackson2JsonMessageConverter() );
+        return template;
     }
 
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 }
--- portal-env/src/main/java/org/example/controller/AnalysisApi.java
+++ portal-env/src/main/java/org/example/controller/AnalysisApi.java
@@ -1,7 +1,9 @@
 package org.example.controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,19 @@ public class AnalysisApi {
 
     private final RabbitTemplate rabbitTemplate;
 
-    @PostMapping("/start")
-    void startAnalysis() {
-        rabbitTemplate.convertAndSend("amq.direct", "event1", "Test Message");
+    @PostMapping("/{analysisId}/start")
+    void startAnalysis(@PathVariable long analysisId) {
+        StartAnalysisCommand startAnalysisCommand = new StartAnalysisCommand( 1, 2, analysisId );
+        rabbitTemplate.convertAndSend("analysis", "analysis.start", startAnalysisCommand);
+    }
+
+
+    @Getter
+    @RequiredArgsConstructor
+    static class StartAnalysisCommand {
+        private final long customerId;
+        private final long userId;
+        private final long analysisId;
     }
 
 }
